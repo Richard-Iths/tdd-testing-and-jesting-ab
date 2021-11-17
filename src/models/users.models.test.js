3b452dddce217ca76874be353376a976d6b8d8a5
thomaswennhall <70705571+thomaswennhall@users.noreply.github.com>
2021-11-17 22:35:53 +0100

    add a simple test for getUsers function

--- src/models/users.models.test.js
+++ src/models/users.models.test.js
@@ -1,1 +1,11 @@
-export default {}
+//import { jest } from '@jest/globals'
+import usersModels from './users.models'
+
+describe('user models', () => {
+  it('should get users from users table', () => {
+    const expected = []
+    const actual = usersModels.getUsers()
+
+    expect(actual).toStrictEqual(expected)
+  })
+})

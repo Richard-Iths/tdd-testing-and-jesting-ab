fdcbb056da5487d1be828eeab1ad1114e0acc6e4
richard-iths <richard.forsberg@iths.se>
2021-11-17 15:54:58 +0100

    should have table for users

--- src/database/database.test.js
+++ src/database/database.test.js
@@ -1,5 +1,11 @@
 import { jest } from "@jest/globals";
+import db from "./db.database";
 
 describe("db.database.js", () => {
-  it("should have table for users", () => {});
+  it("should have table for users", async () => {
+    const expected = "OK";
+    const testData = "users";
+    const result = await db.getFrom(testData);
+    expect(result).toBe(expected);
+  });
 });
---
+++ src/database/db.database.js
@@ -0,0 +1,54 @@
+class Database {
+  instance = null;
+  db = {
+    users: [
+      {
+        id: "990s89da9a9fua9uf9",
+        name: "Anders",
+        password: "grillkorv",
+        login: "grillkorv",
+      },
+    ],
+    products: [
+      {
+        id: "8948108401sjfaj",
+        name: "Nike Shoes",
+        price: 5000,
+      },
+      {
+        id: "ssdada401sjfaj",
+        name: "Nike underwear",
+        price: 500,
+      },
+    ],
+    user_products: [
+      {
+        userId: "990s89da9a9fua9uf9",
+        productId: "8948108401sjfaj",
+        amount: 50,
+      },
+      {
+        userId: "990s89da9a9fua9uf9",
+        productId: "ssdada401sjfaj",
+        amount: 10,
+      },
+    ],
+  };
+  constructor() {
+    if (Database.instance) {
+      return Database.instance;
+    }
+    Database.instance = this;
+  }
+  getFrom(table, query, params) {
+    return new Promise((resolve, reject) => {
+      if (!query || !params) {
+        return this.db[table] && resolve("OK");
+      }
+    });
+  }
+}
+
+const db = new Database();
+
+export default db;

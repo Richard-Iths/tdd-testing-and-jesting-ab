0c84c38753dad13a71d4afeedf0d2c6c189325dc
Azeb.K <54040527+azebkidanemariam@users.noreply.github.com>
2021-11-18 14:21:00 +0100

    wip user model

--- src/models/users.models.test.js
+++ src/models/users.models.test.js
@@ -8,10 +8,22 @@ describe("user models", () => {
 
     expect(actual).toStrictEqual(expected);
   });
-  it("should get users from users table", () => {
+  it("should get single user from users table", () => {
+    const expected = [];
+    const actual = usersModels.getUser("id");
+
+    expect(actual).toStrictEqual(expected);
+  });
+  it("should create user to users table", () => {
     const expected = [];
     const actual = usersModels.createUser("name", "password", "login", "role");
 
+    expect(actual).toStrictEqual(expected);
+  });
+  it("should update user to users table", () => {
+    expect(actual).toStrictEqual(expected);
+  });
+  it("should delete user from users table", () => {
     expect(actual).toStrictEqual(expected);
   });
 });

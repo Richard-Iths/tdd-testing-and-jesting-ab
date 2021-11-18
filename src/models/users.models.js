02f0721c392cdf4071b8f053bb5e19f8bf1c8d6a
Azeb.K <54040527+azebkidanemariam@users.noreply.github.com>
2021-11-18 12:26:56 +0100

    wip usersmodel file

--- src/models/users.models.js
+++ src/models/users.models.js
@@ -1,28 +1,92 @@
-//const db = require('../database')
+import db from "../database";
+import { DataTypes } from "sequelize";
 
+const User = db.define("User", {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  login: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: {
+      args: true,
+      msg: " Username taken",
+    },
+  },
+  role: {
+    type: DataTypes.STRING,
+    enum: ["user", "admin"],
+    defaultValue: "user",
+    allowNull: false,
+  },
+});
+User.authenticate = async (password, login) => {
+  const user = await User.findOne({ where: { login } });
+  if (user) {
+    if (user.password === password) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    throw new Error("User Not Registered");
+  }
+};
+
+//create user to database
+async function createUser(name, login, password, role) {
+  const user = await User.create({ name, password, login, role });
+  if (user) {
+    return { message: "succesfuly register a new user" };
+  } else {
+    throw new Error();
+  }
+}
+
+//loggin a user from the database
+async function login(login, password) {
+  try {
+    const user = await User.authenticate(password, login);
+    if (user) {
+      return { message: "Logged In" };
+    }
+  } catch (error) {
+    return error;
+  }
+}
+
+//get all users from database
 function getUsers() {
-  const users = [] //get all users from database
-  return users
+  const users = User.findAll();
+  return users;
 }
 
+//get a single user from database
 function getUser(id) {
-  const user = {} //get a single user from database
-  return user
+  const user = User.findOne({ where: { id } });
+  return user;
 }
 
-function postUser(name, login, password, role) {
-  //post user to database
-  return { message: 'succesfuly register a new user' }
-}
-
-function deleteUser(id) {
-  //delete user from database
-  return { message: `succesfuly delete user ${id}` }
+//delete user from database
+async function deleteUser(id) {
+  const user = User.findOne({ where: { id } });
+  if (user.UserId !== id) {
+    return false;
+  } else {
+    await user.destroy();
+    return { message: `succesfuly delete user ${id}` };
+  }
 }
 
 export default {
+  login,
   getUsers,
   getUser,
-  postUser,
-  deleteUser
-}
+  createUser,
+  deleteUser,
+};
--- src/models/users.models.test.js
+++ src/models/users.models.test.js
@@ -1,11 +1,17 @@
 //import { jest } from '@jest/globals'
-import usersModels from './users.models'
+import usersModels from "./users.models";
 
-describe('user models', () => {
-  it('should get users from users table', () => {
-    const expected = []
-    const actual = usersModels.getUsers()
+describe("user models", () => {
+  it("should get users from users table", () => {
+    const expected = [];
+    const actual = usersModels.getUsers();
 
-    expect(actual).toStrictEqual(expected)
-  })
-})
+    expect(actual).toStrictEqual(expected);
+  });
+  it("should get users from users table", () => {
+    const expected = [];
+    const actual = usersModels.createUser("name", "password", "login", "role");
+
+    expect(actual).toStrictEqual(expected);
+  });
+});

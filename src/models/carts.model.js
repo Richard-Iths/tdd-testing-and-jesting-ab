b386b918fec735d2d5b34168601e43b453a887f8
Richard <richard.forsberg@iths.se>
2021-11-22 14:05:31 +0100

    added cart model to db

--- src/models/carts.model.js
+++ src/models/carts.model.js
@@ -1,15 +1,5 @@
 export default (sequelize, Sequelize) => {
   const carts = sequelize.define("Cart", {
-    user_id: {
-      type: Sequelize.UUID,
-      references: "Users",
-      referencesKey: "user_id",
-    },
-    products_id: {
-      type: Sequelize.UUID,
-      references: "Products",
-      referencesKey: "product_id",
-    },
     amount: {
       type: Sequelize.NUMBER,
     },

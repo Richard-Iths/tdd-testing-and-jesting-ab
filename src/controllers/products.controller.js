dfc619b0bb55be88499652797d3e784080afed88
Richard <richard.forsberg@iths.se>
2021-11-24 13:02:59 +0100

    added testing products controller

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,15 +1,26 @@
-import db from '../database/db.database.js'
-const { ProductsModel } = db
+import db from "../database/db.database.js";
+const { ProductsModel } = db;
 
 const getAllProducts = async (_, res, next) => {
   try {
-    const products = await ProductsModel.findAll()
-    return res.json({ data: products })
+    const products = await ProductsModel.findAll();
+    return res.json({ data: products });
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
+
+const getProduct = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const product = await ProductsModel.findByPk(id);
+    return res.json({ data: { product } });
+  } catch (error) {
+    next(error);
+  }
+};
 
 export default {
-  getAllProducts
-}
+  getAllProducts,
+  getProduct,
+};

73e45698d49ee05699768cc97ed2aeb0d71ed79e
richard-iths <richard.forsberg@iths.se>
2021-11-25 10:48:20 +0100

    finished tests for products controller

--- src/server.js
+++ src/server.js
@@ -15,6 +15,15 @@ app.get("/", (req, res, next) => {
 });
 
 app.use("/api", router);
+
+app.use((err, req, res, next) => {
+  if (err instanceof BaseException) {
+    return res.status(err.statusCode).json({ data: { message: err.message } });
+  }
+  res
+    .status(500)
+    .json({ data: { message: "Something went wrong please try again" } });
+});
 const server = app.listen(PORT, () => {
   console.log(`Server is running on port:${PORT}`);
 });

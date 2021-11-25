ae86b1ecdfe65b02284ee71b9a59f86a3c5972c1
Richard <richard.forsberg@iths.se>
2021-11-25 14:20:08 +0100

    finished test for products routes

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,10 +1,27 @@
-import { Router } from 'express'
-import productsController from '../controllers/products.controller.js'
-import auth from '../middleware/auth.js'
-const router = new Router()
+import { Router } from "express";
+import productsController from "../controllers/products.controller.js";
+import auth from "../middleware/auth.js";
+const router = new Router();
 
-router.get('/', productsController.getAllProducts)
-router.get('/:id', productsController.getProduct)
-router.post('/', auth.auth, auth.authRole('admin'), productsController.postProduct)
+router.get("/", productsController.getAllProducts);
+router.post(
+  "/",
+  auth.auth,
+  auth.authRole("admin"),
+  productsController.postProduct
+);
+router.get("/:id", productsController.getProduct);
+router.delete(
+  "/:id",
+  auth.auth,
+  auth.authRole("admin"),
+  productsController.deleteProduct
+);
+router.put(
+  "/:id",
+  auth.auth,
+  auth.authRole("admin"),
+  productsController.putProduct
+);
 
-export default router
+export default router;
